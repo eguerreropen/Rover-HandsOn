ee0fa5fa7d0a693f67e2e2323682aa870ff09e97
eguerreropen <eguerreropen@gmail.com>
2026-08-16 17:23:28 -0400

    Delete Codigos directory

--- Codigos/CrearModelo.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolo26n.pt")
-model.export(format="ncnn", imgsz=320)
--- Codigos/OpenDIY.py
+++
@@ -1,5 +0,0 @@
-import cv2
-img = cv2.imread("bus.jpg")
-cv2.imshow("Output",img)
-print(img[500,500,:])
-cv2.waitKey(0)
